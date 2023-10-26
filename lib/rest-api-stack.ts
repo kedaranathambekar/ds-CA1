e1c30c4656f3687d284895c70802ccd5ff500635
kedaranthambekar <20091631@mail.wit.ie>
2023-10-26 15:28:11 +0100

    Add new movie endpoint.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -54,6 +54,17 @@ export class RestAPIStack extends cdk.Stack {
           },
         }
         );
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -74,6 +85,7 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
         
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
@@ -101,7 +113,14 @@ export class RestAPIStack extends cdk.Stack {
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
     );
-        
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );  
+
+    
+   
+      
       }
     }
     
