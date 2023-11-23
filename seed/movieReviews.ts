93b01a4e6e36a4ef9450fca27c785d77e4bcb117
Kedaranath Ambekar <72230613+kedaranathambekar@users.noreply.github.com>
2023-11-23 00:54:58 +0000

    Update movieReviews.ts

--- seed/movieReviews.ts
+++ seed/movieReviews.ts
@@ -1,25 +1,25 @@
 import {MovieReview} from '../shared/types'
 
 export const movieReviews : MovieReview[] = [
-  {
-    movieId: 1234,
-    reviewerName: "hberk",
-    reviewDate: "15/11/2023",
-    reviewContent: "Great movie",
-    reviewRating: 3
-  },
-  {
-    movieId: 12345,
-    reviewerName: "wsmith",
-    reviewDate: "10/10/2023",
-    reviewContent: "Very funny movie. Would recommend",
-    reviewRating: 5
-  },
-  {
-    movieId: 123456,
-    reviewerName: "jdoe",
-    reviewDate: "20/07/2023",
-    reviewContent: "Enjoyed it.",
-    reviewRating: 4
-  }
+   {
+      movieId: 1234,
+      reviewerName: "minions",
+      reviewDate: "30/05/2019",
+      reviewContent: "funny movie",
+      reviewRating: 4
+    },
+    {
+      movieId: 12345,
+      reviewerName: "harry potter",
+      reviewDate: "8/2/2000",
+      reviewContent: "Sci fiction",
+      reviewRating: 5
+    },
+    {
+      movieId: 123456,
+      reviewerName: "the ring",
+      reviewDate: "29/10/1999",
+      reviewContent: "Horror.",
+      reviewRating: 2
+    }
 ];
