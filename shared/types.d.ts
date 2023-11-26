9ecbb132317b1c40f877af2b816e3ae474db633d
Kedaranath Ambekar <72230613+kedaranathambekar@users.noreply.github.com>
2023-11-26 01:45:56 +0000

    Update types.d.ts

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -1,40 +1,13 @@
-import {MovieReview} from '../shared/types'
+export type MovieReview = {
 
-export const movieReviews : MovieReview[] = [
-  
-  {
-    MovieId: 1234,
-    ReviewerName: "kedhar",
-    ReviewDate: "2021-11-16",
-    Content: "Very funny movie. Would recommend",
-    Rating: 1
-},
-{
-    MovieId: 1234,
-    ReviewerName: "kedhar",
-    ReviewDate: "2023-11-17",
-    Content: "Great movie.",
-    Rating: 5
-},
-{
-    MovieId: 1234,
-    ReviewerName: "Kumar",
-    ReviewDate: "2008-06-16",
-    Content: "Enjoyed it.",
-    Rating: 10
-},
-{
-    MovieId: 4400,
-    ReviewerName: "kedhar",
-    ReviewDate: "2003-08-10",
-    Content: "Great movie",
-    Rating: 2
-},
-{
-    MovieId: 8954,
-    ReviewerName: "minions",
-    ReviewDate: "2020-11-25",
-    Content: "horror",
-    Rating: 7
-},
-];
+  MovieId: number;
+  ReviewerName: string;
+  ReviewDate: string;
+  Content: string;
+  Rating: number;  
+}
+export type updateReview = {
+  ReviewDate: string;
+  Content: string;
+  Rating: number;     
+}
