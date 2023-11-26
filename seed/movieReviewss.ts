fe89e0ddc5d30d5f57f9a19db75d0af15806f750
Kedaranath Ambekar <72230613+kedaranathambekar@users.noreply.github.com>
2023-11-26 01:46:47 +0000

    Update movieReviewss.ts

--- seed/movieReviewss.ts
+++ seed/movieReviewss.ts
@@ -1,24 +1,40 @@
 import {MovieReview} from '../shared/types'
-export const movieReviewss : MovieReview[] = [
-    {
-      movieId: 1234,
-      reviewerName: "minions",
-      reviewDate: "30/05/2019",
-      reviewContent: "funny movie",
-      reviewRating: 4
-    },
-    {
-      movieId: 12345,
-      reviewerName: "harry potter",
-      reviewDate: "8/2/2000",
-      reviewContent: "Sci fiction",
-      reviewRating: 5
-    },
-    {
-      movieId: 123456,
-      reviewerName: "the ring",
-      reviewDate: "29/10/1999",
-      reviewContent: "Horror.",
-      reviewRating: 2
-    }
-  ];
+
+export const movieReviews : MovieReview[] = [
+  
+  {
+    MovieId: 1234,
+    ReviewerName: "kedhar",
+    ReviewDate: "2021-11-16",
+    Content: "Very funny movie. Would recommend",
+    Rating: 1
+},
+{
+    MovieId: 1234,
+    ReviewerName: "kedhar",
+    ReviewDate: "2023-11-17",
+    Content: "Great movie.",
+    Rating: 5
+},
+{
+    MovieId: 1234,
+    ReviewerName: "Kumar",
+    ReviewDate: "2008-06-16",
+    Content: "Enjoyed it.",
+    Rating: 10
+},
+{
+    MovieId: 4400,
+    ReviewerName: "kedhar",
+    ReviewDate: "2003-08-10",
+    Content: "Great movie",
+    Rating: 2
+},
+{
+    MovieId: 8954,
+    ReviewerName: "minions",
+    ReviewDate: "2020-11-25",
+    Content: "horror",
+    Rating: 7
+},
+];
